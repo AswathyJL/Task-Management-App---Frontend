b9123f20a37548a054fbdb84f0bbb718decddb70
AswathyJL <aswathyjlal@gmail.com>
2024-11-17 11:34:36 +0530

    second commit

---
+++ src/services/allAPI.js
@@ -0,0 +1,33 @@
+import commonAPI from "./commonAPI"
+import SERVER_URL from "./serverUrl"
+
+// registerAPI - called by auth component
+export const registerAPI = async (reqbody)=>{
+    return await commonAPI("POST",`${SERVER_URL}/register`,reqbody)
+}
+
+// loginAPI called by Auth component when user click login btn
+export const loginAPI = async (reqbody)=>{
+    return await commonAPI("POST",`${SERVER_URL}/login`,reqbody)
+}
+
+// allTaskAPI called by allTasks Component when page loaded in browser
+export const allTaskAPI = async (reqHeader)=>{
+    return await commonAPI("GET",`${SERVER_URL}/all-tasks`,{},reqHeader)
+}
+
+// addTaskAPI - called by alltask component
+export const addTaskAPI = async (reqbody,reqHeader)=>{
+    return await commonAPI("POST",`${SERVER_URL}/add-task`,reqbody,reqHeader)
+}
+
+// editTaskAPI - called by taskCard component
+export const editTaskAPI = async (id,reqbody,reqHeader)=>{
+    return await commonAPI("PUT",`${SERVER_URL}/task/${id}/edit`,reqbody,reqHeader)
+}
+
+// removeTaskAPI - called by taskCard component
+export const removeTaskAPI = async (id,reqHeader)=>{
+    return await commonAPI("PUT",`${SERVER_URL}/task/${id}/remove`,{},reqHeader)
+}
+
